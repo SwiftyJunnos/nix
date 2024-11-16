580fc3b3fd75b46c80cbecf650b3d814201b49c7
Junnos  <junnos.dev@gmail.com>
2024-11-16 22:47:37 +0900

    Add pckgs

--- flake.nix
+++ flake.nix
@@ -19,37 +19,91 @@
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
-            pkgs.nixpkgs-fmt
-            pkgs.mkalias
-            pkgs.warp-terminal
-            pkgs.neovim
+            pkgs.chezmoi
+            pkgs.curl
+            pkgs.discord
             pkgs.fastfetch
+            pkgs.gh
+            pkgs.gifski
+            pkgs.git
+            pkgs.git-lfs
+            pkgs.gitkraken
+            pkgs.gitmoji-cli
+            pkgs.home-assistant-cli
+            pkgs.iterm2
+            pkgs.kubectl
+            pkgs.mas
+            pkgs.mkalias
+            pkgs.mos
+            pkgs.neovim
+            pkgs.nixpkgs-fmt
+            pkgs.obsidian
+            pkgs.postman
+            pkgs.raycast
+            pkgs.slack
+            pkgs.tableplus
+            pkgs.teams
+            pkgs.vscode
+            pkgs.watch
+            pkgs.zoom-us
           ];
 
           homebrew = {
             enable = true;
+            taps = [
+
+            ];
             brews = [
-                "chezmoi"
-                "gh"
-                "gitmoji"
-                "mas"
+
             ];
             casks = [
                 "1password"
                 "1password-cli"
+                "aldente"
                 "betterdisplay"
+                "cursor"
+                "figma"
+                "huly"
                 "iina"
+                "notchnook"
+                "notion"
+                "nvidia-geforce-now"
+                "orbstack"
+                "warp"
+                "zed"
             ];
             masApps = {
+                "Commander One" = 1035236694;
                 "KakaoTalk" = 869223134;
                 "LINE" = 539883307;
-                "Microsoft Teams" = 1113153706;
                 "Microsoft Outlook" = 985367838;
                 "Microsoft Word" = 462054704;
                 "Microsoft Excel" = 462058435;
                 "Microsoft PowerPoint" = 462062816;
             };
             onActivation.cleanup = "zap";
+            onActivation.autoUpdate = true;
+            onActivation.upgrade = true;
+          };
+
+          environment.variables = {
+            EDITOR = "nvim";
+            LANG = "en_KR.UTF-8";
+          };
+
+          system.defaults = {
+            dock.persistent-apps = [
+                "/Applications/Safari.app"
+                "/System/Applications/Mail.app"
+                "/System/Applications/Messages.app"
+                "${pkgs.discord}/Applications/Discord.app"
+                "/Applications/Warp.app"
+                "${pkgs.vscode}/Applications/Visual Studio Code.app"
+                "/Applications/Xcode.app"
+                "${pkgs.obsidian}/Applications/Obsidian.app"
+            ];
+            finder.FXPreferredViewStyle = "clmv";
+            NSGlobalDomain.AppleICUForce24HourTime = true;
           };
 
           fonts.packages = [
